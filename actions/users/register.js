2bd939fff5a7545045ebae91aa87bdaf2f6360d0
José de Jesús Zárate García <jose.jesus.zarate.garcia@outlook.com>
2024-11-13 16:42:49 -0600

    FIC-29 <Funcionalidad registro db>

--- actions/users/register.js
+++ actions/users/register.js
@@ -1,5 +1,22 @@
 "use server"
+import db from "@/libs/db"
+import bcrypt from "bcrypt"
 
 export const register = async (values) =>{
-    
+    const hash = await bcrypt.hash(values.contrasena, 10)
+    try{
+        console.log(values)
+        const response = await db.Usuario.create({
+            data:{
+                correo: values.correo,
+                contrasena: hash,
+                rol: 1  // 1 = Usuario  
+            }        
+        })
+        
+        return response
+    }catch(ex){
+        console.log(ex.message)
+        return {error: ex.message}
+    }
 }
