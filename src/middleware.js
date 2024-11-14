3b89d658048231843e00c855838f7dc4efccc39e
José de Jesús Zárate García <jose.jesus.zarate.garcia@outlook.com>
2024-11-14 13:57:40 -0600

    FIC-30 <Logica del Middleware y pagina de rutas>

--- src/middleware.js
+++ src/middleware.js
@@ -1,12 +1,58 @@
 import authConfig from "@/auth.config"
 import NextAuth from "next-auth"
+import {
+  publicRoutes,
+  authRoutes,
+  medicalRoutes,
+  adminRoutes,
+  apiAuthPrefix,
+  pacientRoutes,
+  DEFAULT_LOGIN_REDIRECT,
+} from "@/routes"
 
 const { auth } = NextAuth(authConfig)
  
 export default auth((req) => {
-   const isLoggedInt = !!req.auth
-   console.log("ROUTE: ", req.nextUrl.pathname)
-   console.log("IS LOGGED IN", isLoggedInt)
+  const {nextUrl} = req
+  const isLoggedIn = !!req.auth
+  const isAdmin = isLoggedIn && req.auth.rol === 3
+  const isMedical = isLoggedIn && req.auth.rol === 2
+  const isPacient = isLoggedIn && req.auth.rol === 1
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
+  const isMedicalRoute = medicalRoutes.includes(nextUrl.pathname)
+  const isAdminRoute = adminRoutes.includes(nextUrl.pathname)
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname)
+  const isPacientRoute = pacientRoutes.includes(nextUrl.pathname)
+
+  if(isApiAuthRoute){
+    return null
+  }
+
+  if(isAuthRoute){
+    if(isLoggedIn){
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+    }
+    return null
+  }
+
+  if(!isLoggedIn && !isPublicRoute){
+    return Response.redirect(new URL("/auth/login", nextUrl))
+  }
+
+  if(isMedicalRoute && !isMedical){
+    return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+  }
+
+  if(isAdminRoute && !isAdmin){
+    return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+  }
+
+  if(isPacientRoute && !isPacient){
+    return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+  }
+
+   
 })
  
 export const config = {
