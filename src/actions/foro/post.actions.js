4f199724df6205350cb5c3c5bd0f21835c87946a
OctavioPeVz <o.pena.v.dev@outlook.com>
2024-11-18 18:59:47 -0600

    Conexion con el back, listado de publicaciones y filtrado por tipo

--- src/actions/foro/post.actions.js
+++ src/actions/foro/post.actions.js
@@ -1,3 +1,4 @@
+"use server"
 import {prisma as db} from "@/libs/db";
 
 
--- src/app/Foro/page.jsx
+++ src/app/Foro/page.jsx
@@ -1,66 +1,11 @@
 "use client"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Image from 'next/image';
 import PostCard from "@/app/components/Foro/PostCard";
-//import PostCard from './components/Foro/PostCard';
+import { getPosts } from "@/actions/foro/post.actions";
+import { getTiposCancer } from "@/actions/tipos_cancer/tiposCancer";
 
 export default function Forum() {
-  const posts = [
-    {
-      id: 1,
-      question: "¿Cuáles son los síntomas iniciales del cáncer de piel?",
-      description:
-        "Estoy notandno una nueva mancha en mi piel. ¿Cómo puedo saber si es un síntoma de alerta o solo algo benigno?",
-      categories: ["Cáncer de piel", "Cáncer"],
-      likes: 120,
-      responses: 30,
-    },
-    {
-      id: 2,
-      question: "¿Qué pruebas recomiendan para la detección temprana de cáncer de mama?",
-      description:
-        "Quiero hacerme estudios para prevenir el cáncer de mama. ¿Qué pruebas son las más recomendadas para una detección temprana?",
-      categories: ["Cáncer de mama", "Cáncer"],
-      likes: 63,
-      responses: 16,
-    },
-    {
-      id: 3,
-      question: "¿Qué pruebas recomiendan para la detección temprana de cáncer de mama?",
-      description:
-        "Quiero hacerme estudios para prevenir el cáncer de mama. ¿Qué pruebas son las más recomendadas para una detección temprana?",
-      categories: ["Cáncer de mama", "Cáncer"],
-      likes: 63,
-      responses: 16,
-    },
-    {
-      id: 4,
-      question: "¿Qué pruebas recomiendan para la detección temprana de cáncer de mama?",
-      description:
-        "Quiero hacerme estudios para prevenir el cáncer de mama. ¿Qué pruebas son las más recomendadas para una detección temprana?",
-      categories: ["Cáncer de colon", "Cáncer"],
-      likes: 63,
-      responses: 16,
-    },
-    {
-      id: 5,
-      question: "¿Qué pruebas recomiendan para la detección temprana de cáncer de mama?",
-      description:
-        "Quiero hacerme estudios para prevenir el cáncer de mama. ¿Qué pruebas son las más recomendadas para una detección temprana?",
-      categories: ["Cáncer de colon", "Cáncer"],
-      likes: 63,
-      responses: 16,
-    },
-    {
-      id: 6,
-      question: "¿Qué pruebas recomiendan para la detección temprana de cáncer de mama?",
-      description:
-        "Quiero hacerme estudios para prevenir el cáncer de mama. ¿Qué pruebas son las más recomendadas para una detección temprana?",
-      categories: ["Cáncer de próstata", "Cáncer"],
-      likes: 63,
-      responses: 16,
-    },
-  ];
 
   const myPosts = [
     { id: 1, question: "¿Cómo prevenir el cáncer de piel?", description: "¿Qué tipo de protector solar recomiendan para prevenir el cáncer de piel en climas cálidos?", categories: ["Cáncer de piel", "Cuidado personal"], likes: 45, responses: 10 },
@@ -76,22 +21,41 @@ export default function Forum() {
     { id: 5, question: "¿Cuáles son los síntomas iniciales del cáncer de piel?", description: "Estoy notando una nueva mancha en mi piel. ¿Cómo puedo saber si es un síntoma de alerta o solo algo benigno?", categories: ["Cáncer de piel", "Cáncer"], likes: 120, responses: 30 },
   ];
 
-  const categories = [
-    "Cáncer de piel",
-    "Cáncer de mama",
-    "Cáncer de colon",
-    "Cáncer de pulmón",
-    "Leucemia",
-    "Cáncer de laringe",
-    "Melanoma",
-    "Cáncer de próstata",
-    "Cáncer de ovario",
-    "Cáncer de hígado",
-    "Cáncer de estómago",
-    "Cáncer de riñón",
-  ];
+  const [posts, setPosts] = useState([]);
+  const [categories, setCategories] = useState([]);
 
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   const [selectedCategory, setSelectedCategory] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [postsResponse, categoriesResponse] = await Promise.all([
+          getPosts(),
+          getTiposCancer(),
+        ]);
+
+        if (postsResponse.OK) {
+          setPosts(postsResponse.data);
+        } else {
+          throw new Error(postsResponse.message || "Error al obtener los posts");
+        }
+
+        if (categoriesResponse.tiposCancer) {
+          setCategories(categoriesResponse.tiposCancer);
+        } else {
+          throw new Error(categoriesResponse.error || "Error al obtener las categorías");
+        }
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const handleCategoryClick = (category) => {
     setSelectedCategory((prev) => (prev === category ? null : category));
@@ -99,8 +63,9 @@ export default function Forum() {
 
   // Filtrar publicaciones según la categoría seleccionada
   const filteredPosts = selectedCategory
-    ? posts.filter((post) => post.categories.includes(selectedCategory))
-    : posts; // Si no hay categoría seleccionada, mostrar todas las publicaciones
+  ? posts.filter((post) => post.Tipo_Cancer?.nombre === selectedCategory)
+  : posts;
+
 
 
   return (
@@ -120,7 +85,7 @@ export default function Forum() {
       <div className="absolute inset-0 bg-gradient-to-r from-white from-8% to-transparent to-40% z-10" />
       <div className="absolute inset-0 bg-gradient-to-l from-white from-8% to-transparent to-40% z-10" />
 
-      <div className="relative max-w-5xl z-10 p-4 rounded-3xl pl-8 pr-8 bg-[#D9D9D9]">
+      <div className="relative max-w-5xl min-w-[1030px] z-10 p-4 rounded-3xl pl-8 pr-8 bg-[#D9D9D9]">
 
         <div className="relative flex items-center space-x-4 mb-4">
           <div className="relative flex-grow">
@@ -133,26 +98,26 @@ export default function Forum() {
         </div>
 
         <div className="flex overflow-x-auto space-x-2 mb-6 pb-4">
-          {categories.map((category, index) => (
+          {categories.map((category) => (
             <button
-              key={index}
-              onClick={() => handleCategoryClick(category)}
-              className={`flex-shrink-0 bg-white text-gray-700 rounded-xl px-4 py-2 text-sm border ${selectedCategory === category ? "bg-[#753350] text-gray-700 border-[#753350]" : "border-gray-300"}`}
+              key={category.id}
+              onClick={() => handleCategoryClick(category.nombre)}
+              className={`flex-shrink-0 bg-white text-gray-700 rounded-xl px-4 py-2 text-sm border ${selectedCategory === category.nombre ? "bg-[#753350] text-gray-700 border-[#753350]" : "border-gray-300"}`}
             >
-              {category}
+              {category.nombre}
             </button>
           ))}
         </div>
 
         <h1 className="text-3xl font-bold pl-1 mb-6 text-[#753350]">Post en Tendencia</h1>
 
-        <div className="h-[660px] overflow-y-scroll pr-4 scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100">
+        <div className="h-[660px] min-h-[660px] overflow-y-scroll pr-4 scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100">
           {filteredPosts.map((post) => (
             <PostCard
               key={post.id}
-              question={post.question}
-              description={post.description}
-              categories={post.categories}
+              question={post.titulo}
+              description={post.cuerpo}
+              categories={post.Tipo_Cancer.nombre}
               likes={post.likes}
               responses={post.responses}
               compact={false}
@@ -195,7 +160,7 @@ export default function Forum() {
 
         {/* Posts Guardados */}
         <div className="h-[40%] bg-[#D9D9D9] rounded-3xl p-6 shadow-lg">
-          
+
           <div className="flex justify-between items-center mb-4">
             <h3 className="text-lg font-semibold text-[#753350]">Post Guardados</h3>
             <button className="text-sm text-[#753350] hover:underline">Ver todos</button>
