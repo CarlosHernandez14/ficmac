1a2b9fe76631c965477b5c9996a76d463d9ac7d7
Alejandro Gutiérrez Barrera <106762139+Guty90@users.noreply.github.com>
2024-11-18 18:26:29 -0600

    FIC-34 <Tipos cancer back integrado 100%>

--- src/app/components/TiposCancer/TiposCancer.jsx
+++ src/app/components/TiposCancer/TiposCancer.jsx
@@ -1,10 +1,13 @@
+"use client"
 import React from 'react'
 import MenuTipoCancer from './MenuTiposCancer/MenuTipoCancer'
 import Sintomas from './Sintomas/Sintomas'
 import TipoCancer from './TipoCancer/TipoCancer'
 import InfoTipoCancer from './TipoCancer/InfoTipoCancer'
+import { useState } from 'react'
 
 function TiposCancer() {
+  const [canceres, setCanceres] = useState([]);
   return (
     <div className='relative'>
       <div className='absolute inset-0'>
@@ -12,11 +15,11 @@ function TiposCancer() {
       </div>
       <div className='relative z-10 p-4'>
         <div className='flex justify-evenly items-center'>
-          <MenuTipoCancer />
+          <MenuTipoCancer setCanceres={setCanceres}/>
           <div className='space-y-2'>
-            <TipoCancer />
-            <InfoTipoCancer />
-            <Sintomas />
+            <TipoCancer nombre={canceres.nombre}/>
+            <InfoTipoCancer img={canceres.path_imagen} descripcion={canceres.descripcion}/>
+            <Sintomas sintomas={canceres.Tipo_Cancer_Sintoma}/>
           </div>
         </div>
       </div>
