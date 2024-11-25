3fc36b8bfe2247811b63dddc32c0211b9aa53512
JovanyEquihua <equicerjoal@gmail.com>
2024-11-25 00:17:10 -0600

    FIC-66 página principal 70%
    Carrusel
    Nuestros números

---
+++ src/app/components/Home/Carrusel.jsx
@@ -0,0 +1,65 @@
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+const Carrusel = ({ slides }) => {
+  return (
+    <div className="relative h-screen">
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        pagination={{ clickable: true }}
+        navigation
+        autoplay={{ delay: 3000 }}
+        loop={true}
+        className="h-full"
+      >
+        {slides.map((slide, index) => (
+          <SwiperSlide key={index}>
+            <div
+              className="h-screen flex flex-col justify-center items-start text-white relative"
+              style={{
+                backgroundImage: `url(${slide.imagen})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+            >
+              <div
+                className="absolute inset-0 pointer-events-none"
+                style={{
+                  background: `linear-gradient(${slide.degradadoDireccion}, ${slide.colorDegradado}, rgba(0, 0, 0, 0))`,
+                  zIndex: 1,
+                }}
+              ></div>
+              <div
+                className={`z-10 p-8 ml-5 w-1/3  ${
+                  slide.textoAlineacion === "derecha"
+                    ? "text-right"
+                    : "text-left"
+                }`}
+              >
+                <h1 className="text-xl font-medium mb-4 text-justify ">
+                  {slide.texto}
+                </h1>
+                <h1 className="text-2xl font-bold mt-10 mb-4 text-justify">
+                  {slide.texto2}
+                </h1>
+                <button
+                  className={`px-16 py-2 mt-10 bg-white  hover:bg-[#CB1662] rounded-[20px] hover:text-white text-[#CB1662] font-bold ${
+                    slide.botonPosicion === "derecha" ? "ml-auto" : "mr-auto"
+                  }`}
+                >
+                  {slide.botonTexto}
+                </button>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default Carrusel;
---
+++ src/app/components/Home/NuestrosNumeros.jsx
@@ -0,0 +1,110 @@
+import React from "react";
+import Cifras from "./Cifras";
+
+const NuestrosNumeros = () => {
+  return (
+    <div className="relative p-5">
+      <img
+        src="/Home/Carrusel_vista_3_Y_Fondo.jpg"
+        alt="Fondo"
+        className="absolute inset-0 w-full h-full object-cover opacity-20"
+      />
+      <div className="relative z-10 ">
+        <h1 className="text-center font-extrabold text-[#CB1662] text-2xl ">
+          Nuestros números
+        </h1>
+        <h1 className="text-center font-extrabold text-[#367B99] text-3xl mt-10">
+          Cifras qué hablan por si solas
+        </h1>
+
+        <div className="flex flex-wrap gap-4 items-center justify-center mt-32">
+          <Cifras
+            imagen={"/Home/icono_pacientes.png"}
+            numeros={"22.000"}
+            texto={
+              <span>
+                Pacientes
+                <br /> <strong className="font-bold">evaluados</strong>
+              </span>
+            }
+            altImagen={"pacientes evaluados icono"}
+          />
+          <Cifras
+            imagen={"/Home/icono_pruebas.png"}
+            numeros={"92.000"}
+            texto={
+              <span>
+                Pruebas
+                <br /> <strong className="font-bold">ejecutadas</strong>
+              </span>
+            }
+            altImagen={"pruebas evaluados icono"}
+          />
+          <Cifras
+            imagen={"/Home/icono_muestras_2.png"}
+            numeros={"9.000"}
+            texto={
+              <span>
+                Muestras actuales
+                <br /> en el <strong className="font-bold">biobanco</strong>
+              </span>
+            }
+            altImagen={"muestras evaluados icono"}
+          />
+          <Cifras
+            imagen={"/Home/icono_muestras_1.png"}
+            numeros={"12.000"}
+            texto={
+              <span>
+                Muestras actuales
+                <br /> en <strong className="font-bold">seroteca</strong>
+              </span>
+            }
+            altImagen={"muestras actuales icono"}
+          />
+          <Cifras
+            imagen={"/Home/icono_articulos.png"}
+            numeros={"154"}
+            texto={
+              <span>
+                Artículos
+                <strong className="font-bold">
+                {" "} publicados
+                  <br />
+                </strong>
+                en la última década
+              </span>
+            }
+            altImagen={"artículos publicados icono"}
+          />
+          <Cifras
+            imagen={"/Home/icono_grant.png"}
+            numeros={"+ 15"}
+            texto={
+              <span>
+                Grants nacionales e<br /> internacionales
+                <strong className="font-bold"> ejecutados</strong>
+              </span>
+            }
+            altImagen={"grants ejecutados icono"}
+          />
+          <Cifras
+            imagen={"/Home/vida_icono.png"}
+            numeros={"+ 98.000"}
+            texto={
+              <span>
+                Años de vida <strong className="font-bold">ganados</strong>
+                <br /> gracias al diagnóstico
+                <br />
+                de precisión
+              </span>
+            }
+            altImagen={"años de vida ganados icono"}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NuestrosNumeros;
