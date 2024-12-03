c52e6dc4995cc63369936e19c05e4afdbdf0aa89
Alejandro Gutiérrez Barrera <106762139+Guty90@users.noreply.github.com>
2024-12-03 17:06:41 -0600

    FIC-101 <CRUD investigaciones y dashboard admin>

---
+++ src/app/components/Admin/Dashboard/ItemDashboard.jsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import React from "react";
+
+function ItemDashboard({ icon: Icon, name, href, className, onClick }) {
+  return (
+    <Link
+      href={href}
+      className={`text-xl hover:bg-[#a13c68] rounded-xl p-2 flex items-center space-x-3 text-white  ${className}`}
+      onClick={onClick}
+    >
+      <Icon />
+      <span>{name}</span>
+    </Link>
+  )
+}
+
+export default ItemDashboard
