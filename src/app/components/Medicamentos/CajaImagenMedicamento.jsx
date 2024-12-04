51c053e33e06883859b2c95104c1ef0eff0fff88
JudithMart <aguimtz.2003@gmail.com>
2024-12-03 23:38:23 -0600

    FIC-93<medicamentos-frontend-back-100%>

--- src/app/components/Medicamentos/CajaImagenMedicamento.jsx
+++ src/app/components/Medicamentos/CajaImagenMedicamento.jsx
@@ -4,8 +4,8 @@ function CajaImagenMedicamento({tipo}) {
     return (
       <div className="bg-[#CB1662] w-80 h-auto p-5 rounded-e-xl">
         <img
-          src={tipo.path_imagen}
-          alt={tipo.nombre}
+          src={tipo}
+          alt={"Medicamento"}
           className="rounded-2xl w-full h-full object-cover "
         />
       </div>
--- src/app/components/Medicamentos/CajaTexto.jsx
+++ src/app/components/Medicamentos/CajaTexto.jsx
@@ -1,32 +1,27 @@
 import React from 'react'
+import CajaImagenMedicamento from "./CajaImagenMedicamento";
 
 function CajaTexto({ tipo}) {
   return (
-    <div className=" space-y-5 p-10">
-      <p className="text-[#CB1662] font-bold text-4xl">{tipo.nombre}Cisplatino</p>
+    <div className='flex justify-between'>
+      <div className=" space-y-5 p-10">
+        <p className="text-[#CB1662] font-bold text-4xl">{tipo.nombre}</p>
 
-      <p className="font-bold text-[#CB1662] text-2xl ">
-        Uso:
-        <span className="ml-2 font-bold text-black text-xl">
-          {tipo.uso}Se usa en el tratamiento de cáncer de pulmón, ovario,testículo,
-          vejiga, cabeza y cuello
-        </span>
-      </p>
+        <p className="font-bold text-[#CB1662] text-2xl ">
+          Descripción:
+          <span className="ml-2 font-bold text-black text-xl">
+            {tipo.descripcion}
+          </span>
+        </p>
 
-      <p className="font-bold text-[#CB1662] text-2xl ">
-        Mecanismo:
-        <span className=" ml-2 font-bold text-black text-xl">
-          {tipo.mecanismo}Se usa en el tratamiento de cáncer de pulmón,
-          ovario,testículo, vejiga, cabeza y cuello
-        </span>
-      </p>
-      <p className="font-bold text-[#CB1662] text-2xl ">
-        Efectos secundarios:
-        <span className=" ml-2 font-bold text-black text-xl">
-          {tipo.efectos}Se usa en el tratamiento de cáncer de pulmón,
-          ovario,testículo, vejiga, cabeza y cuello
-        </span>
-      </p>
+        <p className="font-bold text-[#CB1662] text-2xl ">
+          Efectos secundarios:
+          <span className=" ml-2 font-bold text-black text-xl">
+            {tipo.efectos_secundarios}
+          </span>
+        </p>
+      </div>
+      <CajaImagenMedicamento tipo={tipo.url_imagen} />
     </div>
   );
 }
