cadf454621879d255f7853b0010e2f565fce927d
JovanyEquihua <equicerjoal@gmail.com>
2024-12-06 10:59:17 -0600

    FIC-126 Sidebar
    * Solo falta poner las rutas

--- src/app/Medicos/layout.js
+++ src/app/Medicos/layout.js
@@ -1,11 +1,13 @@
 import FooterGeneral from "../components/Footer/FooterGeneral";
 import NavbarGeneral from "../components/Navbar/NavbarGeneral";
 import FlotadorDonacion from "../components/Donacion/FlotadorDonacion";
+import SidebarMedicos from "../components/Medicos/Dashboard/SidebarMedicos";
 
 export default function MedicosLayout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen">
-      <main className="flex-grow mt-24">{children}</main>
+    <div className="flex min-h-screen">
+      <SidebarMedicos/>
+      <main className="flex-grow ">{children}</main>
     </div>
   );
 }
---
+++ src/app/components/Medicos/Dashboard/SidebarMedicos.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { FaUserCircle, FaStethoscope, FaMicroscope, FaComments } from 'react-icons/fa';
+import { FaHospitalUser, FaVialCircleCheck  } from "react-icons/fa6";
+import NavbarItemGeneral from '../../Navbar/NavbarItemGeneral'
+
+function SidebarMedicos() {
+  return (
+    <div className='bg-[#753350] w-auto px-8 h-screen'>
+        <div className='p-4 flex justify-center'>
+            <img src='\General\logoblanco.png' alt='imagen' className='w-20 h-8'/>
+        </div>
+        <div className='flex flex-col items-center space-y-4 my-12'>
+            <FaUserCircle className='text-white text-9xl'/>
+            <p className='text-white text-xl'>Médicos</p>
+        </div>
+        <div className='flex flex-col items-center text-justify space-y-2'>
+            <NavbarItemGeneral icon={FaHospitalUser} name='Pacientes' href='' />
+            <NavbarItemGeneral  icon={FaComments} name='Foro' href='' />
+            <NavbarItemGeneral icon={FaVialCircleCheck} name='Resultados' href='/Medicos/ResultadosEstudios' />
+            <NavbarItemGeneral icon={FaStethoscope} name='Solicitudes' href='/Medicos/ConsultarEstudios' />
+
+        </div>
+    </div>
+  )
+}
+
+export default SidebarMedicos
